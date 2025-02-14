3173aa7f92c976db8a70e89dced67aeef6449828
shadabb-josh <shadab.shikalgar@joshsoftware.com>
2025-02-14 16:43:24 +0530

    fixes

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
-import './App.css'
 import Todo from './components/Todo'
+import './App.css'
 
 function App() {
 
--- src/components/Todo.tsx
+++ src/components/Todo.tsx
@@ -1,16 +1,16 @@
-import { useState } from "react";
+import React, { useState } from "react";
+import Table from "./Table";
+import { ADD } from "../constants";
 
-interface Todo {
+export interface Todo {
   id: number;
   todo: string;
   checked: boolean;
 }
 
-// const todoObject: todo = {};
-
 const Todo: React.FC = () => {
-  const [todos, SetTodos] = useState<Todo[]>([]);
-  const [text, SetText] = useState<string>("");
+  const [todos, setTodos] = useState<Todo[]>([]);
+  const [text, setText] = useState<string>("");
 
   const addTodo = () => {
     const todoObj: Todo = {
@@ -18,71 +18,45 @@ const Todo: React.FC = () => {
       todo: text,
       checked: false,
     };
-    SetTodos([...todos, todoObj]);
-    SetText("");
+    setTodos([...todos, todoObj]);
+    setText("");
   };
 
   const deleteTodo = (id: number) => {
-    SetTodos(todos.filter((todo) => todo.id != id));
+    setTodos(todos.filter((todo) => todo.id !== id));
+  };
+
+  const setTodoText = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setText(e.target.value);
   };
 
   const toggleCheck = (id: number) => {
-    SetTodos(
+    setTodos(
       todos.map((todo) =>
         todo.id === id ? { ...todo, checked: !todo.checked } : todo
       )
     );
   };
+
   return (
     <div>
       <div className="mb-3">
-        <label htmlFor="exampleFormControlInput1" className="form-label">
-          What you are going TO-DO ?
+        <label htmlFor="todoInput" className="form-label">
+          What you are going TO-DO?
         </label>
         <input
           type="text"
           className="form-control"
-          id="exampleFormControlInput1"
-          placeholder="What are you gonna do today ?"
+          id="todoInput"
+          placeholder="What are you gonna do today?"
           value={text}
-          onChange={(e) => SetText(e.target.value)}
+          onChange={(e) => setTodoText(e)}
         />
         <button className="mt-2 btn btn-primary" onClick={addTodo}>
-          Add
+          {ADD}
         </button>
       </div>
-      <table className="table">
-        <thead>
-          <tr>
-            <th scope="col">#</th>
-            <th scope="col">TODO</th>
-            <th scope="col">Actions</th>
-          </tr>
-        </thead>
-        <tbody>
-          {todos.map((todo) => (
-            <tr>
-              <th scope="row">{todo.id}</th>
-              <td className={todo.checked ? "checked" : ""}>{todo.todo}</td>
-              <td>
-                <input
-                  className="form-check-input"
-                  type="checkbox"
-                  id="flexCheckDefault"
-                  style={{ marginRight: "10px", marginTop: "7px" }}
-                  onChange={() => toggleCheck(todo.id)}
-                />
-                <button
-                  className="btn btn-danger"
-                  onClick={() => deleteTodo(todo.id)}
-                >
-                  Delete
-                </button>
-              </td>
-            </tr>
-          ))}
-        </tbody>
-      </table>
+      <Table todos={todos} toggleCheck={toggleCheck} deleteTodo={deleteTodo} />
     </div>
   );
 };
