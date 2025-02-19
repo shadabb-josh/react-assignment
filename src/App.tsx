19a63333af7a167e229ba2fae0a750bc8ebbcd69
shadabb-josh <shadab.shikalgar@joshsoftware.com>
2025-02-19 17:17:19 +0530

    add assignment 3

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,51 @@
-import './App.css'
+import { useState } from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import TodoForm from "./components/TodoForm";
+import TodoList from "./components/TodoList";
+import "./App.css";
 
-function App() {
-
-  return (
-    <>
-      <h1>Hello World! </h1>
-    </>
-  )
+export interface Todo {
+  id: number;
+  todo: string;
+  date: string;
+  checked: boolean;
 }
 
-export default App
+function App() {
+  const [todos, setTodos] = useState<Todo[]>([]);
+
+  const addTodo = (todoText: string, date: string) => {
+    setTodos((prevTodos) => [
+      ...prevTodos,
+      { id: prevTodos.length + 1, todo: todoText, date, checked: false },
+    ]);
+  };
+
+  const deleteTodo = (id: number) => {
+    setTodos((prevTodos) => prevTodos.filter((todo) => todo.id !== id));
+  };
+
+  const toggleCheck = (id: number) => {
+    setTodos((prevTodos) =>
+      prevTodos.map((todo) =>
+        todo.id === id ? { ...todo, checked: !todo.checked } : todo
+      )
+    );
+  };
+
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<TodoForm addTodo={addTodo} />} />
+        <Route
+          path="/todos"
+          element={
+            <TodoList todos={todos} toggleCheck={toggleCheck} deleteTodo={deleteTodo} />
+          }
+        />
+      </Routes>
+    </Router>
+  );
+}
+
+export default App;
